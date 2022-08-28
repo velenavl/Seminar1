7255db91342ff7f9f527e8bcc23eb69ca6ea6a4d
Elena <velenavl@inbox.ru>
2022-08-28 17:57:15 +0300

    Ex 003

---
+++ Ex003/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int number = Convert.ToInt32(Console.ReadLine()); // Конвертация в число
+
+int resulte = number * number;
+
+Console.WriteLine(resulte);
